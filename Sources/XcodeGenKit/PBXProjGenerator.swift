e34d3350222894c4f616a4233f794c00c4d6cfa0
Hector Matos <hectormatos2011@gmail.com>
2019-07-30 13:21:02 -0700

    modernize project and include Base in localization

--- Sources/XcodeGenKit/PBXProjGenerator.swift
+++ Sources/XcodeGenKit/PBXProjGenerator.swift
@@ -207,7 +207,7 @@ public class PBXProjGenerator {
             .merged(project.attributes)
 
         let knownRegions = sourceGenerator.knownRegions.sorted()
-        pbxProject.knownRegions = knownRegions.isEmpty ? ["en"] : knownRegions
+        pbxProject.knownRegions = knownRegions.isEmpty ? ["en, Base"] : knownRegions
 
         let allTargets: [PBXTarget] = Array(targetObjects.values) + Array(targetAggregateObjects.values)
         pbxProject.targets = allTargets
